22308a9c04623ba172bc13608a9eafd2c25dda37
Todd Pihl <pihltd@users.noreply.github.com>
2024-10-14 20:58:09 -0400

    Create CertInstall.sh

---
+++ CertInstall.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/pihl/.acme.sh/acme.sh --install-cert --domain pihlsecor.duckdns.org --cert-file /home/pihl/homeassistant/cert.pem --key-file /home/pihl/homeassistant/key.pem --fullchain-file /home/pihl/homeassistant/fullchain.pem
